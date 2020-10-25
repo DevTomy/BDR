ef1adef79d499fdf8175721ba26bef6002fd34bc
DevTomy <48916069+DevTomy@users.noreply.github.com>
2020-10-25 15:55:39 +0330

    exmple file ( Updated )

--- index.php
+++ index.php
@@ -7,12 +7,12 @@ $str="Hello";
 
 // Hashing Method
 echo "Hashed : \n";
-echo $bdr->hash($str)."\n\n";
+echo $Bdr->hash($str)."\n\n";
 
 // Encoding Method
 echo "Encoded : \n";
-echo $bdr->encode($str)."\n\n";
+echo $Bdr->encode($str)."\n\n";
 
 // Decoding Mrthod
 echo "Decoded : \n";
-echo $bdr->decode('|33|16|35|39|55|16|24|64|'); // |33|16|35|39|55|16|24|64| => Hello
+echo $Bdr->decode('|33|16|35|39|55|16|24|64|'); // |33|16|35|39|55|16|24|64| => Hello
